05d4a8edfd8771e0f23b490feb04879b6c682626
seefs001 <xiaojian8886@gmail.com>
2021-04-23 13:14:17 +0800

    update xzip

--- xzip/xzip.go
+++ xzip/xzip.go
@@ -10,153 +10,125 @@ import (
 	"strings"
 )
 
-// source:http://liupzmin.com/2019/08/02/golang/working-zip-archive/
-
-//Zip 压缩为zip格式
-//zipit("/tmp/report.txt", "/tmp/report-2015.zip", "*.log")
-//source为要压缩的文件或文件夹, 绝对路径和相对路径都可以
-//target是目标文件
-//filter是过滤正则(Golang 的 包 path.Match)
-func Zip(source, target, filter string) error {
-	var err error
-	if isAbs := filepath.IsAbs(source); !isAbs {
-		source, err = filepath.Abs(source) // 将传入路径直接转化为绝对路径
-		if err != nil {
-			return err
-		}
-	}
-	//创建zip包文件
-	zipfile, err := os.Create(target)
+func Zip(dst, src string) (err error) {
+	// 创建准备写入的文件
+	fw, err := os.Create(dst)
+	defer fw.Close()
 	if err != nil {
 		return err
 	}
 
+	// 通过 fw 来创建 zip.Write
+	zw := zip.NewWriter(fw)
 	defer func() {
-		if err := zipfile.Close(); err != nil {
-			log.Println("*File close error: %s, file: %s", err.Error(), zipfile.Name())
-		}
-	}()
-
-	//创建zip.Writer
-	zw := zip.NewWriter(zipfile)
-
-	defer func() {
+		// 检测一下是否成功关闭
 		if err := zw.Close(); err != nil {
-			log.Println("zipwriter close error: %s", err.Error())
+			log.Fatalln(err)
 		}
 	}()
 
-	info, err := os.Stat(source)
-	if err != nil {
-		return err
-	}
-
-	var baseDir string
-	if info.IsDir() {
-		baseDir = filepath.Base(source)
-	}
-
-	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
-
-		if err != nil {
-			return err
+	// 下面来将文件写入 zw ，因为有可能会有很多个目录及文件，所以递归处理
+	return filepath.Walk(src, func(path string, fi os.FileInfo, errBack error) (err error) {
+		if errBack != nil {
+			return errBack
 		}
 
-		//将遍历到的路径与pattern进行匹配
-		ism, err := filepath.Match(filter, info.Name())
-
+		// 通过文件信息，创建 zip 的文件信息
+		fh, err := zip.FileInfoHeader(fi)
 		if err != nil {
-			return err
+			return
 		}
-		//如果匹配就忽略
-		if ism {
+
+		// 替换文件信息中的文件名
+		fh.Name = strings.TrimPrefix(path, string(filepath.Separator))
+
+		// 这步开始没有加，会发现解压的时候说它不是个目录
+		if fi.IsDir() {
+			fh.Name += "/"
+		}
+
+		// 写入文件信息，并返回一个 Write 结构
+		w, err := zw.CreateHeader(fh)
+		if err != nil {
+			return
+		}
+
+		// 检测，如果不是标准文件就只写入头信息，不写入文件数据到 w
+		// 如目录，也没有数据需要写
+		if !fh.Mode().IsRegular() {
 			return nil
 		}
-		//创建文件头
-		header, err := zip.FileInfoHeader(info)
+
+		// 打开要压缩的文件
+		fr, err := os.Open(path)
+		defer fr.Close()
 		if err != nil {
-			return err
+			return
 		}
 
-		if baseDir != "" {
-			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
-		}
-
-		if info.IsDir() {
-			header.Name += "/"
-		} else {
-			header.Method = zip.Deflate
-		}
-		//写入文件头信息
-		writer, err := zw.CreateHeader(header)
+		// 将打开的文件 Copy 到 w
+		_, err = io.Copy(w, fr)
 		if err != nil {
-			return err
+			return
 		}
+		// 输出压缩的内容
+		//fmt.Printf("成功压缩文件： %s, 共写入了 %d 个字符的数据\n", path, n)
 
-		if info.IsDir() {
-			return nil
-		}
-		//写入文件内容
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-
-		defer func() {
-			if err := file.Close(); err != nil {
-				log.Println("*File close error: %s, file: %s", err.Error(), file.Name())
-			}
-		}()
-		_, err = io.Copy(writer, file)
-
-		return err
+		return nil
 	})
-
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
-//Unzip 解压zip
-// unzip("/tmp/report-2015.zip", "/tmp/reports/")
-func Unzip(archive, target string) error {
-	reader, err := zip.OpenReader(archive)
+func UnZip(dst, src string) (err error) {
+	// 打开压缩文件，这个 zip 包有个方便的 ReadCloser 类型
+	// 这个里面有个方便的 OpenReader 函数，可以比 tar 的时候省去一个打开文件的步骤
+	zr, err := zip.OpenReader(src)
+	defer zr.Close()
 	if err != nil {
-		return err
+		return
 	}
 
-	if err := os.MkdirAll(target, 0755); err != nil {
-		return err
+	// 如果解压后不是放在当前目录就按照保存目录去创建目录
+	if dst != "" {
+		if err := os.MkdirAll(dst, 0755); err != nil {
+			return err
+		}
 	}
 
-	for _, file := range reader.File {
-		unzippath := filepath.Join(target, file.Name)
+	// 遍历 zr ，将文件写入到磁盘
+	for _, file := range zr.File {
+		path := filepath.Join(dst, file.Name)
+
+		// 如果是目录，就创建目录
 		if file.FileInfo().IsDir() {
-			err := os.MkdirAll(unzippath, file.Mode())
-			if err != nil {
+			if err := os.MkdirAll(path, file.Mode()); err != nil {
 				return err
 			}
+			// 因为是目录，跳过当前循环，因为后面都是文件的处理
 			continue
 		}
 
-		fileReader, err := file.Open()
+		// 获取到 Reader
+		fr, err := file.Open()
 		if err != nil {
 			return err
 		}
-		defer fileReader.Close()
 
-		targetFile, err := os.OpenFile(unzippath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		// 创建要写出的文件对应的 Write
+		fw, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, file.Mode())
 		if err != nil {
 			return err
 		}
-		defer targetFile.Close()
 
-		if _, err := io.Copy(targetFile, fileReader); err != nil {
+		_, err = io.Copy(fw, fr)
+		if err != nil {
 			return err
 		}
+
+		// 将解压的结果输出
+		//fmt.Printf("成功解压 %s ，共写入了 %d 个字符的数据\n", path, n)
+
+		fw.Close()
+		fr.Close()
 	}
-
 	return nil
 }
